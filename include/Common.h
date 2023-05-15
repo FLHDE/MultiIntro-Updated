48833c1e3154dda1871a37ef23e7647f10f63711
C.S. Melis <cees3331x00@gmail.com>
2023-05-15 23:56:28 +0200

    Add INI_Reader definitions

--- include/Common.h
+++ include/Common.h
@@ -8,4 +8,20 @@ namespace Universe
 	IMPORT uint get_base_id(char const *);
 }
 
+class IMPORT INI_Reader
+{
+public:
+    bool open(char const *, bool);
+    bool read_header(void);
+    bool is_header(char const *);
+    bool read_value(void);
+    bool is_value(char const *);
+    bool get_value_bool(unsigned int);
+    int get_value_int(unsigned int);
+    void close(void);
+
+private:
+    char data[0x1568];
+};
+
 #endif
